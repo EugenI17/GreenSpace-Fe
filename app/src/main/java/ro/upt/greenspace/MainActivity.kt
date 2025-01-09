c20904c2ca55801c1143139ceb3ca5c9b00e0eed
Eugen Ionescu <vladionescu511@gmail.com>
2025-01-09 23:08:38 +0200

    added open camera button

--- app/src/main/java/ro/upt/greenspace/MainActivity.kt
+++ app/src/main/java/ro/upt/greenspace/MainActivity.kt
@@ -1,15 +1,23 @@
 package ro.upt.greenspace
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ro.upt.greenspace.ui.theme.GreenSpaceFeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,25 +27,39 @@ class MainActivity : ComponentActivity() {
             GreenSpaceFeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    GreenPage()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun openCamera() {
+        val intent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivity(intent)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GreenSpaceFeTheme {
-        Greeting("Android")
+    @Composable
+    fun GreenPage() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Green),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = { openCamera() },
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Add New Plant")
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreenPagePreview() {
+        GreenSpaceFeTheme {
+            GreenPage()
+        }
     }
 }
